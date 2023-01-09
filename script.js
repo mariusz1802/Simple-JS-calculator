fb1a5b0fe97eba7aa4abc1c20ff04cb1b0b23d93
Mariusz <mariusz.janowski@yahoo.pl>
2023-01-09 18:52:17 +0100

    add Top digital

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ let buttonArr = [];
 for (let i = 0; i <= 9; i++) {
   const button = document.querySelector(`#but${i}`);
   buttonArr.push(button);
+  buttonArr;
 }
 
 let numbers = [];
 let singleNumber = [];
+let digitNumber;
 
 function createSingleNumber() {
   for (let i = 0; i < buttonArr.length; i++) {
@@ -16,17 +18,35 @@ function createSingleNumber() {
         return;
       } else {
         singleNumber.push(`${i}`);
-        const digitNumber = Number(singleNumber.join(""));
+        digitNumber = Number(singleNumber.join(""));
         bottomDigital.innerHTML = digitNumber;
       }
     });
   }
 }
 
-function deletNumber() {
+//addEventListener for every key
+addEventListener("keydown", (event) => {
+  console.log(event.key);
+});
+
+function createSingleNumber() {
+  for (let i = 0; i < buttonArr.length; i++) {
+    buttonArr[i].addEventListener("click", () => {
+      if (singleNumber.length > 10) {
+        return;
+      } else {
+        singleNumber.push(`${i}`);
+        digitNumber = Number(singleNumber.join(""));
+        bottomDigital.innerHTML = digitNumber;
+      }
+    });
+  }
+}
+
+function deleteNumber() {
   const deletButton = document.querySelector("#butCE");
   deletButton.addEventListener("click", () => {
-    console.log("CE");
     singleNumber = [];
     bottomDigital.innerHTML = 0;
   });
@@ -34,15 +54,21 @@ function deletNumber() {
 
 function addNumbers() {
   const plusButton = document.querySelector(`#butPlus`);
-  let topDigital = document.querySelector(".topDigital");
-  plusButton.addEventListener("click", () => {
-    numbers.push(bottomDigital);
-    topDigital.innerHTML = bottomDigital.innerHTML + "+";
-    singleNumber = [];
-    bottomDigital.innerHTML = 0;
-  });
+  plusButton.addEventListener("click", topDigitPlus);
 }
 
+const topStringArr = [];
+
+function topDigitPlus() {
+  let topDigital = document.querySelector(".topDigital");
+  topStringArr.push(digitNumber);
+  topDigital.innerHTML = topStringArr.join("+");
+  singleNumber = [];
+  bottomDigital.innerHTML = 0;
+}
+
+function equal() {}
+
 createSingleNumber();
-deletNumber();
+deleteNumber();
 addNumbers();
