c4b57059bf2963aff86e401c875351bc56e8d8c9
Mariusz <mariusz.janowski@yahoo.pl>
2023-01-18 18:24:51 +0100

    finish calculator project

--- script.js
+++ script.js
@@ -11,16 +11,18 @@ class Calculator {
     this.operation = undefined;
   }
 
-  delete() {}
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+
+  }
   appendNumber(number) {
     if (number === '.' && this.currentOperand.includes('.')) return;
-
     this.currentOperand = this.currentOperand.toString() + number.toString();
   }
 
   chooseOperation(operation) {
-    if (this.currentOperand === '')return;
-    if(this.previousOperand !== ''){
+    if (this.currentOperand === '') return;
+    if (this.previousOperand !== '') {
       this.compute();
     }
     this.operation = operation;
@@ -29,34 +31,37 @@ class Calculator {
   }
 
   compute() {
-let computation;
-const prev = parseFloat(this.previousOperand);
-const current = parseFloat(this.currentOperand);
-if(isNaN(prev) || isNaN(current)) return
-      switch(this.opearation){
-        case '+':
-          computation = prev + current
-          break;
-          case '-':
-            computation = prev - current
-            break;
-            case '*':
-              computation = prev * current
-              break;
-              case '÷':
-                computation = prev / current
-                break;
-                default:
-                  return
-      }
-      this.currentOperand = computation
-      this.operation = undefined;
-      this.previousOperand = ''
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch(this.operation) {
+      case '+':
+        computation = prev + current;
+        break;
+      case '-':
+        computation = prev - current;
+        break;
+      case '*':
+        computation = prev * current;
+        break;
+      case '÷':
+        computation = prev / current; 
+        break;
+      default:
+        return;
+    }
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = '';
   }
 
   updateDisplay() {
     this.currentOperandTextElement.innerText = this.currentOperand;
     this.previousOperandTextElement.innerText = this.previousOperand;
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation}`
+    }
   }
 }
 
@@ -91,8 +96,19 @@ operationButtons.forEach((button) => {
   });
 });
 
-
-equalsButton.addEventListener('click', button => {
+equalsButton.addEventListener('click', (button) => {
   calculator.compute();
-  calculator.updateDisplay()
-} )
+  calculator.updateDisplay();
+});
+
+dataAC.addEventListener('click', (button) => {
+  console.log("clear")
+  calculator.clear();
+  calculator.updateDisplay();
+});
+
+
+dataDel.addEventListener('click', (button) => {
+  calculator.delete()
+  calculator.updateDisplay();
+})
